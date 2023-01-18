f2d141f78f9fa8905ffa82057b2eee418c444f6f
Craig <indigomx9@gmail.com>
2023-01-18 01:46:12 -0500

    Updated the Home component.

--- src/containers/home/Home.tsx
+++ src/containers/home/Home.tsx
@@ -1,16 +1,50 @@
 import React from "react";
 import "./Home.css";
+import { motion } from "framer-motion";
 
 export const Home = (): JSX.Element => {
     return (
         <React.Fragment>
-            <section id="home" className="app__section">
-                <h1>Home</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur atque molestias harum, explicabo sunt nemo quaerat saepe repudiandae id ducimus beatae nesciunt impedit totam quia a, eaque reprehenderit quo quam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur atque molestias harum, explicabo sunt nemo quaerat saepe repudiandae id ducimus beatae nesciunt impedit totam quia a, eaque reprehenderit quo quam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur atque molestias harum, explicabo sunt nemo quaerat saepe repudiandae id ducimus beatae nesciunt impedit totam quia a, eaque reprehenderit quo quam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur atque molestias harum, explicabo sunt nemo quaerat saepe repudiandae id ducimus beatae nesciunt impedit totam quia a, eaque reprehenderit quo quam.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur atque molestias harum, explicabo sunt nemo quaerat saepe repudiandae id ducimus beatae nesciunt impedit totam quia a, eaque reprehenderit quo quam.</p>
+            <section id="home" 
+                className="app__section">
+                <motion.p
+                    className="intro__text"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ 
+                        duration: 1, 
+                        ease: "easeIn"
+                    }}
+                >hello, my name is
+                </motion.p>
+                <motion.h1
+                    className="my__name large__head"
+                    initial={{ y: 50, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ 
+                        duration: 1, 
+                        ease: "easeIn", 
+                        delay: 1
+                    }}
+                >Craig Johnson
+                </motion.h1>
+                <motion.h5
+                    className="title large__head"
+                    initial={{ y: 50, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{
+                        duration: 1, 
+                        ease: "easeIn", 
+                        delay: 1.2
+                    }}
+                >I am a Developer.
+                </motion.h5>
+                <motion.p
+                    className="description"
+                >I am a web developer based out of the 
+                    Metropolitan New York area with 
+                    a passion for computer technology.
+                </motion.p>
             </section>
         </React.Fragment>
     );
